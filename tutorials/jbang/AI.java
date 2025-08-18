e10e4f9be876064001356df49fa797559c54f944
Michael Vorburger <vorburger@google.com>
2025-08-18 22:27:46 +0200

    feat: JBang! 💥 🤯

---
+++ tutorials/jbang/AI.java
@@ -0,0 +1,13 @@
+//JAVA 17+
+//PREVIEW
+//REPOS enola=https://docs.enola.dev/maven-repo/,mavencentral,jitpack
+//DEPS com.google.adk:google-adk-dev:0.2.1-SNAPSHOT
+//
+// All this ^^^ should not be removed because https://JBang.dev needs this...
+
+import com.google.adk.agents.LlmAgent;
+import com.google.adk.web.AdkWebServer;
+
+void main() {
+    AdkWebServer.start(LlmAgent.builder().name("AI").model("gemini-2.0-flash").instruction("Be very grumpy!").build());
+}
